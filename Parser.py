e7127a37c4f61784879e4e7a953769161179dc4d
= <=>
2019-03-13 18:45:47 +0400

    Some code refactoring

--- Parser.py
+++ Parser.py
@@ -16,19 +16,13 @@ class Parser():
         return {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     def handle_proxy(self):
-        if self.use_proxy:
-            return self.create_tree_with_proxy()
-        else:
             return self.create_tree()
 
     def create_tree(self):
         headers = self.get_headers()
         page = requests.get(self.link, headers=headers)
-        return html.fromstring(page.content.decode('utf-8'))
-
-    def create_tree_with_proxy(self):
-        headers = self.get_headers()
-        page = requests.get(self.link, headers=headers, proxies=self.proxy())
+        if self.use_proxy:
+            page = requests.get(self.link, headers=headers, proxies=self.proxy())
         return html.fromstring(page.content.decode('utf-8'))
 
     def get_data(self, html_code):
